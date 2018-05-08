aa28de20bf0371d44f7246e3af71f748ca99c338
ConnorODonnell <connor.odonnell625@gmail.com>
2018-05-08 14:00:30 -0700

    Initial Commit

---
+++ app/controllers/map.js
@@ -0,0 +1,31 @@
+import Controller from '@ember/controller';
+
+function testAlert (){
+  alert("You Win");
+}
+
+export default Controller.extend({
+  updateCheck: '',
+  locCoordinates: '',
+  locGive: '',
+  locName: '',
+  latitude: '',
+  longitude: '',
+  lat: 33.876118,
+  lng: -117.921410,
+  zoom: 13,
+
+  actions: {
+    updateMap(){
+      this.set('lat', this.get('latitude'));
+      this.set('lng', this.get('longitude'));
+      this.set('locName', this.get('locGive'));
+      this.set('locGive', '');
+      this.set('latitude', '');
+      this.set('longitude', '');
+      this.set('updateCheck', 'true');
+      this.set('locCoordinates', [this.get('lat'), this.get('lng')]);
+      this.set('zoom', 20);
+    }
+  }
+});
